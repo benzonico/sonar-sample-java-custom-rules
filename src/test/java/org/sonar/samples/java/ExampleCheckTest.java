17be0c123075bba20eb258f20d7afbe1f81751c7
Nicolas PERU <nicolas.peru@sonarsource.com>
2014-02-28 16:03:19 +0100

    Add comments to the test class

--- src/test/java/org/sonar/samples/java/ExampleCheckTest.java
+++ src/test/java/org/sonar/samples/java/ExampleCheckTest.java
@@ -9,6 +9,11 @@ import org.sonar.squid.api.SourceFile;
 
 import java.io.File;
 
+/**
+ * This class is the test of the ExampleCheck.
+ * We test a check by running it against a minimal valid file to raise the issue and
+ * verify we raise the correct number of violation with correct messages.
+ */
 public class ExampleCheckTest {
 
 
@@ -17,10 +22,14 @@ public class ExampleCheckTest {
 
   @Test
   public void detected() {
+
+    //Parse a known file and use an instance of the check under test to raise issue.
     SourceFile file = JavaAstScanner
         .scanSingleFile(new File("src/test/files/ExampleCheck.java"), new VisitorsBridge(new ExampleCheck()));
 
+    //Verify the messages raised by the check
     checkMessagesVerifier.verify(file.getCheckMessages())
+        //Expect an issue at line 9 with a predefined message. This allows test of dynamic messages.
         .next().atLine(9).withMessage("Issue raised on method.");
   }
 }
